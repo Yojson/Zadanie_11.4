bc6051a5ab72991ca6d72a520093300342129cad
Yojson <maxkuster888@gmail.com>
2025-11-12 09:03:16 +0100

    Edit

--- app/src/main/java/com/example/zadanie_114/MainActivity.java
+++ app/src/main/java/com/example/zadanie_114/MainActivity.java
@@ -2,14 +2,12 @@ package com.example.zadanie_114;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,16 +28,19 @@ public class MainActivity extends AppCompatActivity {
         Age = findViewById(R.id.Age);
         button = findViewById(R.id.button);
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = Name.getText().toString();
+                String age = Age.getText().toString();
 
-        button.setOnClickListener(v -> {
-            String name = Name.getText().toString();
-            String age = Age.getText().toString();
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra(EXTRA_NAME, name);
+                intent.putExtra(EXTRA_AGE, age);
 
-            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-            intent.putExtra(EXTRA_NAME, name);
-            intent.putExtra(EXTRA_AGE, age);
-
-            startActivity(intent);
+                startActivity(intent);
+            }
         });
     }
 }
+
